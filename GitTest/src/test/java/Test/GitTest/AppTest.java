3b0c42fa907ded48f0eda018e067dba293090095
aneeshvly <aneeshvly@gmail.com>
2021-05-07 14:10:49 +0530

    first commit

---
+++ GitTest/src/test/java/Test/GitTest/AppTest.java
@@ -0,0 +1,15 @@
+package Test.GitTest;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+	@Test
+    public void testApp()
+    {
+        System.out.println("\n\n Sample Git \n\n ");
+    }
+}
